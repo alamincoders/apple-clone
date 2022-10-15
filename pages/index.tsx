fdefc972196717c73d93792899d99023438fa764
alamincoders <developeralamin03@gmail.com>
2022-10-15 17:29:28 +0600

    tap section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import type { GetServerSideProps, NextPage } from "next";
+import { Tab } from "@headlessui/react";
+import type { GetServerSideProps } from "next";
 import Head from "next/head";
 import Landing from "../components/Landing";
-import TabsCard from "../components/TabsCard";
 import { fetchCategories } from "../utils/fetchCategories";
 
-const Home: NextPage = () => {
+interface Props {
+  categories: Category[];
+}
+
+const Home = ({ categories }: Props) => {
+  console.log(categories);
   return (
     <div>
       <Head>
@@ -17,7 +22,30 @@ const Home: NextPage = () => {
         <section className="relative z-40 min-h-screen bg-[#1b1b1b]">
           <div className="space-y-10 py-16">
             <h1 className="text-center text-4xl font-medium tracking-wide text-white md:text-5xl">New Promos</h1>
-            <TabsCard />
+
+            <Tab.Group>
+              <Tab.List className="flex justify-center">
+                {categories?.map((category) => (
+                  <Tab
+                    key={category._id}
+                    id={category._id}
+                    className={({ selected }) =>
+                      `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                        selected ? "borderGradient !bg-[#35383c] text-white" : "border-b-2 border-[#35383c] text-[#747474]"
+                      }`
+                    }
+                  >
+                    {category?.title}
+                  </Tab>
+                ))}
+              </Tab.List>
+              <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+                {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+        <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+        <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+        <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
+              </Tab.Panels>
+            </Tab.Group>
           </div>
         </section>
       </main>
@@ -28,10 +56,12 @@ const Home: NextPage = () => {
 export default Home;
 
 // Backend code
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const categories = await fetchCategories();
 
   return {
-    props: {},
+    props: {
+      categories,
+    },
   };
 };
