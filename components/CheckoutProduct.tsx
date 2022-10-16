bdc2754a174c28fcc076e9e9bd359bdae30a77e6
alamincoders <developeralamin03@gmail.com>
2022-10-16 12:55:23 +0600

    checkout page

---
+++ components/CheckoutProduct.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import Currency from "react-currency-formatter";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { removeFromBasket } from "../redux/feature/basketSlice";
+import { urlFor } from "../sanity";
+
+interface Props {
+  items: Product[];
+  id: string;
+}
+
+const CheckoutProduct = ({ items, id }: Props) => {
+  const dispatch = useDispatch();
+
+  const removeItemFromBasket = () => {
+    dispatch(removeFromBasket({ id }));
+    toast.error(`${items[0].title} removed to basket`, {
+      position: "bottom-center",
+    });
+  };
+
+  const basketTotal = items.reduce((total, item) => total + item.price, 0);
+
+  return (
+    <div className="flex flex-col gap-x-4 border-b border-gray-300 pb-5 lg:flex-row lg:items-center">
+      <div className="relative h-44 w-44">
+        <Image src={urlFor(items[0].image[0]).url()} layout="fill" objectFit="contain" alt="product image" />
+      </div>
+      <div className="flex flex-1 items-end lg:items-center">
+        <div className="flex-1 space-y-4">
+          <div className=" flex flex-col gap-x-8 text-xl lg:flex-row lg:text-2xl">
+            <h4 className="font-semibold lg:w-96">{items[0].title}</h4>
+            <p className="flex items-end cursor-pointer gap-x-1 font-semibold">
+              {items.length}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-blue-500"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+              </svg>
+            </p>
+          </div>
+          <p className="flex cursor-pointer items-end text-blue-500 hover:underline">
+            Show Product Details
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-blue-500"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+          </p>
+        </div>
+        <div className="flex flex-col items-end space-y-4 ">
+          <h4 className="text-xl font-semibold lg:text-2xl">
+            <Currency quantity={basketTotal} currency="USD" />
+          </h4>
+          <button onClick={removeItemFromBasket} className="text-blue-500 hover:underline">
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutProduct;
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import emptyImg from "../assets/images/empty.gif";
+import Button from "../components/Button";
+import CheckoutProduct from "../components/CheckoutProduct";
+import { selectBasketItems } from "../redux/feature/basketSlice";
 
 const Checkout = () => {
-    return (
-        <div>
-            
+  const items = useSelector(selectBasketItems);
+  const router = useRouter();
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState({} as { [key: string]: Product[] });
+
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item._id] = results[item._id] || []).push(item);
+      return results;
+    }, {} as { [key: string]: Product[] });
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
+
+  return (
+    <div className="min-h-screen overflow-hidden bg-[#e7ecee] ">
+      <Head>
+        <title>Bag - Apple</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+
+      <main className="mx-auto mx-w-5xl pb-24">
+        <div className="text-center px-5">
+          <h1 className="my-4 text-3xl font-semibold lg:text-4xl">
+            {" "}
+            {items.length > 0 ? (
+              "Review your bag."
+            ) : (
+              <div>
+                <h2>Your bag is empty</h2>
+                <div className="w-96 h-96 mx-auto">
+                  <Image src={emptyImg} alt="empty basket" />
+                </div>
+              </div>
+            )}
+          </h1>
+          <p className="my-4">Free delivery and free returns.</p>
+          {items.length === 0 && <Button title="Continue Shopping" onClick={() => router.push("/")} />}
         </div>
-    );
+
+        {/* item group */}
+        {items.length > 0 && (
+          <div className="mx-5 md:mx-8">
+            {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+              <CheckoutProduct key={key} items={items} id={key} />
+            ))}
+          </div>
+        )}
+      </main>
+    </div>
+  );
 };
 
 export default Checkout;
